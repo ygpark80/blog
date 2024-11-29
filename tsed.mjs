ba93ca218f30e91ab0368a123242eb310973affb
YG Park <ygpark80@gmail.com>
2024-11-29 15:41:33 +0900

    Update tsed.mjs

--- tsed.mjs
+++ tsed.mjs
@@ -2,12 +2,19 @@ try { await $`rm -rf dist/myproject` } catch {}
 await $`mkdir -p dist/myproject`
 cd("dist/myproject")
 
-await $`npx -p @tsed/cli tsed init --features swagger,testing,jest -s .`
+await $`npx -p @tsed/cli tsed init --features swagger,testing,vitest --package-manager pnpm -s .`
 
-await $`yarn build`
+await $`pnpm run build`
 
 const packageJson = await fs.readJson("./package.json")
-packageJson.scripts.test = "yarn run test:coverage"
+packageJson.scripts.test = "pnpm run test:coverage"
 await fs.writeJson("./package.json", packageJson, { spaces: 2, EOL: "\n" })
 
-await $`yarn test --coverageThreshold '{}'`
+const specFile = await fs.readFile("src/Server.integration.spec.ts", "utf8")
+const updatedContent = specFile.replace(
+  'import { PlatformTest } from "@tsed/platform-http";',
+  'import { PlatformTest } from "@tsed/platform-http/testing";'
+)
+await fs.writeFile("src/Server.integration.spec.ts", updatedContent)
+
+await $`pnpm run test`
